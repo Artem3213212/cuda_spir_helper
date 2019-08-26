0784552edeb57b4ea535fca9104bad92a793538e
Alexey-t <support@uvviewsoft.com>
2019-08-26 12:02:02 +0300

    use list comprehens

--- db.py
+++ db.py
@@ -6,18 +6,22 @@ filename_db=os.path.join(os.path.dirname(__file__), 'spirv.core.grammar.json')
 def load_db(fn):
     db={}
     db_enums={}
+
     with open(fn,'r') as f:
         json_db=json.loads(f.read())
-    for i in json_db.get('instructions',[]):
-        itm=[]
-        for i0 in i.get('operands',[]):
-            itm.append(i0['kind'])
-        db[i['opname']]=itm
-    for i in json_db.get('operand_kinds',[]):
-        itm=[]
-        for i0 in i.get('enumerants',[]):
-            itm.append(i0['enumerant'])
-        db_enums[i['kind']]=itm
+
+    try:
+        for i in json_db['instructions']:
+            db[i['opname']] = [i0['kind'] for i0 in i.get('operands',[])]
+    except:
+        pass
+
+    try:
+        for i in json_db['operand_kinds']:
+            db_enums[i['kind']] = [i0['enumerant'] for i0 in i.get('enumerants',[])]
+    except:
+        pass
+
     return db,db_enums
 
 db,db_enums=load_db(filename_db)
